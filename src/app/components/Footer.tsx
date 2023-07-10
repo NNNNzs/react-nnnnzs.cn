94e7384832ec8df2ef08f00da3034e3cac9f4374
NNNNzs <nnnnzs@vip.qq.com>
2023-07-10 23:50:55 +0800

    chore: 更新footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,3 +1,81 @@
+interface Social {
+  href: string;
+  title: string;
+  componentName: string;
+}
+
+const social: Social[] = [
+  {
+    href: "https://github.com/NNNNzs",
+    title: "Github",
+    componentName: "icon-github-fill",
+  },
+  {
+    href: "https://weibo.com/NNNNzs",
+    title: "微博",
+    componentName: "icon-weibo",
+  },
+  {
+    href: "https://www.instagram.com/nnnn2s/",
+    title: "instagram",
+    componentName: "icon-instagram",
+  },
+  {
+    href: "https://twitter.com/NNNNzs",
+    title: "twitter",
+    componentName: "icon-google",
+  },
+  {
+    href: "https://www.zhihu.com/people/NNNNzs",
+    title: "知乎",
+    componentName: "icon-zhihu",
+  },
+];
+const YEAR = new Date().getFullYear();
+
 export default function Footer() {
-  return <></>;
+  return (
+    <footer className="bg-gray-800 text-gray-400 py-4 mt-8">
+      <div className="social">
+        <ul
+          className="
+        flex
+        m-auto
+        w-1/5
+        max-auto
+        justify-around
+        items-center
+        text-center
+        margin-auto
+      "
+        >
+          {social.map((s) => {
+            return (
+              <li key={s.href} className="cursor-pointer" title={s.title}>
+                <a href={s.href} target="_blank">
+                  <i
+                    className={`iconfont text-4xl text-white w-4 h-4 ${s.componentName}`}
+                  ></i>
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="copyright text-center">
+        <p>©2021 - {YEAR}, content by NNNNzs. All Rights Reserved.</p>
+        <p>
+          <a href="https://nextjs.org/" target="_blank">
+            Next13{" "}
+          </a>
+        </p>
+        <p>
+          <a href="https://beian.miit.gov.cn" target="_blank">
+            皖ICP备16025009号-1
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
 }
